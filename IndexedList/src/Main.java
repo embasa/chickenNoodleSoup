a47bf9d739cf62ff01bbf144646622b35795790f
Bruno Osvaldo Hernandez Esquivel <brunoh77@outlook.com>
2015-06-22 18:11:54 -0700

    added tests in Main of assignment

--- IndexedList/src/Main.java
+++ IndexedList/src/Main.java
@@ -7,65 +7,115 @@ import java.util.Random;
  * Created by computerito on 6/12/15.
  */
 public class Main {
-    public static void main(String[] args){
-        int N = 100000;
 
-        /** Make set of N number of to test **/
-        Random rand = new Random( System.currentTimeMillis() );
-        long start = System.currentTimeMillis();
-        LinkedList<Integer> arrayList = new LinkedList<>();
-        arrayList.add(10);
-        for( int i=0;i<N;i++ ){
-            int value = rand.nextInt(arrayList.size());
-            arrayList.add(value,rand.nextInt());
+    private static ArrayList<Integer> arrayList;
+    private static LinkedList<Integer> linkedList;
+    private static IndexedList indexedList;
+    private static Random rand;
+    public static void initLists(int n){
+        rand = new Random(System.currentTimeMillis());
+        indexedList = new IndexedList();
+        arrayList = new ArrayList<>(n);
+        for(int i = 0; i < n; i++){
+            indexedList.add(0);
+            arrayList.add(0);
         }
-        long elapsed = System.currentTimeMillis() - start;
-        System.out.print("elapsed: " + elapsed + "\n");
-        /**
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        for( int i=0;i<N;i++ ){
-            linkedList.add( arrayList.get(i) );
-        }
-
-        IndexedList myList = new IndexedList();
-        System.out.print("Initial value\n");
-        for( int i=0;i<N;i++ ){
-            myList.add( arrayList.get(i) );
-        }
-        int iterations = 10;
-        int count = 0;
-        int sum = 0;
-        System.out.print("indexedList:\n");
-        while( count++<iterations ){
-            sum += testGet(myList);
-        }
-        System.out.print("average time elasped: " + sum/iterations + "\n");
-        count = 0;
-        sum = 0;
-        System.out.print("linkedList:\n");
-        while( count++<iterations ){
-            sum += testGet(linkedList);
-        }
-        System.out.print("average time elasped: " + sum/iterations + "\n");
-        count = 0;
-        sum = 0;
-        System.out.print("arrayList:\n");
-        while( count++<iterations ){
-            sum += testGet(arrayList);
-        }
-        System.out.print("average time elasped: " + sum/iterations + "\n");
-         **/
+        linkedList = new LinkedList<>(arrayList);
     }
 
-    public static long testGet(List<Integer> list){
-        Random rand = new Random( System.currentTimeMillis() );
-        int N = 10000;
-        int iterations = 1000000;
+    private static long timeAddRandomly(List<Integer> lst){
         long start = System.currentTimeMillis();
-        for( int i = iterations;i > 0;--i ){
-            int val = rand.nextInt(N);
-            list.get(val);
+        int n = lst.size();
+        for(int i = 0; i< n; i++ ){
+            lst.add(rand.nextInt(lst.size()),rand.nextInt());
         }
+
         return System.currentTimeMillis() - start;
+    }
+
+    private static long timeElementAccess(List<Integer> lst){
+        long start = System.currentTimeMillis();
+
+        for(int i = 0; i< lst.size(); i++ ){
+            Integer elmt = lst.get(i);
+        }
+
+        return System.currentTimeMillis() - start;
+    }
+
+    private static long timeElementRemoval(List<Integer> lst){
+        long start = System.currentTimeMillis();
+
+        for(int i = 0; i< lst.size(); i++ ){
+            Integer elmt = lst.remove(rand.nextInt(lst.size()));
+        }
+
+        return System.currentTimeMillis() - start;
+    }
+
+    private static long timeSetElement(List<Integer> lst){
+        long start = System.currentTimeMillis();
+
+        for(int i = 0; i< lst.size(); i++ ){
+            Integer elmt = lst.set(rand.nextInt(lst.size()), rand.nextInt());
+        }
+
+        return System.currentTimeMillis() - start;
+    }
+
+    public static void main(String[] args){
+        runTests();
+    }
+
+    public static void runTests(){
+        int N = 100000;
+        System.out.print("Test get():\n");
+        System.out.printf("%8s%15s%15s%15s\n", "n", "ArrayList", "LinkedList", "IndexedList");
+        for( int n = 100; n <= N ; n*=10 ){
+            initLists(n);
+
+            long timeArray = timeElementAccess(arrayList);
+            long timeList = timeElementAccess(linkedList);
+            long timeIndexed = timeElementAccess(indexedList);
+
+            System.out.printf("%8d%12d ms%12d ms%12d ms\n", n,timeArray,timeList,timeIndexed);
+        }
+
+        System.out.print("\nTest set():\n");
+        System.out.printf("%8s%15s%15s%15s\n","n","ArrayList","LinkedList","IndexedList");
+        for( int n = 100; n <= N ; n*=10 ){
+            initLists(n);
+
+            long timeArray = timeSetElement(arrayList);
+            long timeList = timeSetElement(linkedList);
+            long timeIndexed = timeSetElement(indexedList);
+
+            System.out.printf("%8d%12d ms%12d ms%12d ms\n", n,timeArray,timeList,timeIndexed);
+        }
+
+        System.out.print("\nTest remove():\n");
+        System.out.printf("%8s%15s%15s%15s\n","n","ArrayList","LinkedList","IndexedList");
+        for( int n = 100; n <= N ; n*=10 ){
+            initLists(n);
+
+            long timeArray = timeElementRemoval(arrayList);
+            long timeList = timeElementRemoval(linkedList);
+            long timeIndexed = timeElementRemoval(indexedList);
+
+            System.out.printf("%8d%12d ms%12d ms%12d ms\n", n,timeArray,timeList,timeIndexed);
+        }
+
+        System.out.print("\nTest add():\n");
+        System.out.printf("%8s%15s%15s%15s\n","n","ArrayList","LinkedList","IndexedList");
+        for( int n = 100; n <= N ; n*=10 ){
+            initLists(n/2);//fill up to half, then use function to add the second half
+
+            long timeArray = timeAddRandomly(arrayList);
+            long timeList = timeAddRandomly(linkedList);
+            long timeIndexed = timeAddRandomly(indexedList);
+
+            System.out.printf("%8d%12d ms%12d ms%12d ms\n", n,timeArray,timeList,timeIndexed);
+        }
+
     }
 }
